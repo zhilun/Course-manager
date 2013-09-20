012d60ec23730a4750f21b07b7637153f65f49e0
zhilun <trankynam2612@gmail.com>
2013-09-20 17:19:23 +0700

    Page 88

--- coursemanager/src/main/java/com/tutorial/model/CourseTypeEnum.java
+++
@@ -1,6 +0,0 @@
-package com.tutorial.model;
-
-public enum CourseTypeEnum {
-
-    SEMINAR, CREDIT, CONTINUING_EDUCATION
-}
--- coursemanager/src/test/java/com/tutorial/model/CourseDataOnDemand.java
+++
@@ -1,6 +0,0 @@
-package com.tutorial.model;
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Course.class)
-public class CourseDataOnDemand {
-}
